4e56468b6e1833f7772bdde9c74769401adb7f00
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-03-26 10:12:27 +0700

    update path

--- windows-service.js
+++ windows-service.js
@@ -2,7 +2,7 @@ const { Service } = require("node-windows");
 const svc = new Service({
   name: "Turnstile App",
   description: "Turnstile app as a Windows service.",
-  script: "C:\\path\\to\\your\\turnstile\\app.js",
+  script: "C:\\apps\\turnstile\\app.js",
 });
 
 svc.on("install", () => {
