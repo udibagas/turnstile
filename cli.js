953dcce86babf0a27fdbb95a44e1c6347742fa8d
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-04-05 09:34:06 +0700

    update cli

--- cli.js
+++ cli.js
@@ -1,10 +1,26 @@
 require("dotenv").config();
+const { Op } = require("sequelize");
+const { Ticket } = require("./models");
 const fetchTicket = require("./lib/fetchTicket");
 const [command] = process.argv.slice(2);
 
 switch (command) {
   case "ticket:fetch":
-    (async () => await fetchTicket())();
+    fetchTicket();
+    break;
+
+  case "ticket:prune":
+    Ticket.destroy({
+      where: {
+        ticket_status: {
+          [Op.ne]: "ready",
+        },
+      },
+    })
+      .then((deletedRows) => {
+        console.log(`Deleted ${deletedRows} rows`);
+      })
+      .catch((err) => console.log(err));
     break;
 
   default:
